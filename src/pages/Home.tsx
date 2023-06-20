901cefa6025ffdaedfec0551585c30aca1bd8686
ilkaybasboga <ilkaybasboga1701@gmail.com>
2023-06-20 09:22:37 +0300

    fourth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -120,3 +120,125 @@ const Home = () => {
 };
 
 export default Home;
+
+
+        //    <Card
+                //               shadow="xl"
+                //               radius="lg"
+                //               p="lg"
+                //               withBorder
+                //               sx={(theme) => ({
+                //                 backgroundColor:
+                //                   theme.colorScheme === "dark"
+                //                     ? theme.colors.dark[6]
+                //                     : theme.colors.gray[0],
+                //                 cursor: "pointer",
+                //                 display: "flex",
+                //                 justifyContent: "space-between",
+                //                 alignItems: "center",
+
+                //                 "&:hover": {
+                //                   backgroundColor:
+                //                     theme.colorScheme === "dark"
+                //                       ? theme.colors.dark[5]
+                //                       : theme.colors.gray[1],
+                //                 },
+                //               })}
+                //             >
+                //               <TbArticle size={26} />
+                //               <Text w={200} truncate fz="lg">
+                //                 {present.description}
+                //               </Text>
+                //               <TbTrash size={26} />
+                //             </Card>
+           //     ))}
+
+             /* <Tabs.Panel value="videos" pt="lg">
+          <SimpleGrid
+            cols={3}
+            spacing="lg"
+            breakpoints={[
+              { maxWidth: "62rem", cols: 3, spacing: "md" },
+              { maxWidth: "48rem", cols: 2, spacing: "sm" },
+              { maxWidth: "36rem", cols: 1, spacing: "sm" },
+            ]}
+          >
+            {mockdata.videos &&
+              mockdata?.videos.map((videos) => (
+                <Card
+                  shadow="xl"
+                  radius="lg"
+                  p="lg"
+                  withBorder
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[0],
+                    cursor: "pointer",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[1],
+                    },
+                  })}
+                >
+                  <TbVideo size={26} />
+                  <Text w={200} truncate fz="lg">
+                    {videos.description}
+                  </Text>
+                  <TbTrash size={26} />
+                </Card>
+              ))}
+          </SimpleGrid>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="models" pt="xl">
+          <SimpleGrid
+            cols={3}
+            spacing="lg"
+            breakpoints={[
+              { maxWidth: "62rem", cols: 3, spacing: "md" },
+              { maxWidth: "48rem", cols: 2, spacing: "sm" },
+              { maxWidth: "36rem", cols: 1, spacing: "sm" },
+            ]}
+          >
+            {mockdata.models &&
+              mockdata?.models.map((models) => (
+                <Card
+                  shadow="xl"
+                  radius="lg"
+                  p="lg"
+                  withBorder
+                  sx={(theme) => ({
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[0],
+                    cursor: "pointer",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+
+                    "&:hover": {
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[5]
+                          : theme.colors.gray[1],
+                    },
+                  })}
+                >
+                  <TbBoxModel size={26} />
+                  <Text w={200} truncate fz="lg">
+                    {models.description}
+                  </Text>
+                  <TbTrash size={26} />
+                </Card>
+              ))}
+          </SimpleGrid>
+        </Tabs.Panel> */
